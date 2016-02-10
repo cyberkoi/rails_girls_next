ad0d4b22e157126adfcde4e62d712dfe6e8f338b
Sarah Ni <snjqi188@gmail.com>
2016-02-10 01:21:07 +0000

    configuring application.rb

--- Gemfile
+++ Gemfile
@@ -1,12 +1,20 @@
 source 'https://rubygems.org'
 
+# allows us to use Sinatra DSL (Domain Specific Lang) in our app
 gem "sinatra"
+# run automated tasks, like creating and migrating the database
 gem "rake"
+# is the database we will be using to store data
 gem "sqlite3"
+# is the interface the app uses to communicate with the database
 gem "activerecord"
+# is a bridge that allows us to use Active Record in a Sinatra application
 gem "sinatra-activerecord"
 
 group :development do
+  # allows us to interact with the datavase through the command line
   gem "shotgun"
+  # will reload our app, so we don't have to stop and start the server whenever 
+  # a minor change is made
   gem "tux"
 end
---
+++ config/application.rb
@@ -0,0 +1,18 @@
+# require gems and classes
+
+# enables our application to automatically discover the Gemfile
+require 'bundler'
+# loads into the project all the gems that are specified in the Gemfile
+Bundler.require 
+# adds the entire project to $LOAD_PATH. This allows Sinatra to find all files 
+# you've added to your project
+$: << File.expand.path('../', __FILE__)
+# Explicitly requires each file found in our model, view and controller folders
+Dir['./app/**/*.rb'].sort.each { |file| require file }
+
+# configure sinatra
+# the last three lines set the root of the project and tells the application 
+# where the erb (embedded Ruby) files and CSS files are located
+set :root, Dir['./app']
+set :public_folder, Proc.new { File.join(root, 'assets') }
+set :erb, :layout => :'layouts/application'
